f9bb4635c679af4ebe8fd2a83e5067c67d5789cf
rafaelpin <skynet.repo@gmail.com>
2022-06-10 12:32:02 -0300

    Add project files.

---
+++ funcionarios.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_Funcionarios
+{
+    class funcionarios
+    {
+
+      public   int Id { get; set; }
+      public  string Name { get; set; }
+      public  int Salario { get; set; }
+
+        public funcionarios (int id, string name, int salario)
+        {
+            Id = id;
+            Name = name;
+            Salario = salario
+        }
+ 
+    
+    }
+}
